4324737de91a592fcae8999753ceb87647d23870
Nicolas Denoël <nicolas.denoel@gmail.com>
2020-07-26 18:20:54 +0200

    Part 3 completed

---
+++ 03-IntroducingLists/3.1-3.3-Lists.py
@@ -0,0 +1,20 @@
+# 3.1
+names = ['john', 'dirk', 'elon', 'rita', 'bob']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+# 3.2
+print(f'Hello, {names[0].title()}')
+print(f'Hello, {names[1].title()}')
+print(f'Hello, {names[2].title()}')
+print(f'Hello, {names[3].title()}')
+print(f'Hello, {names[4].title()}')
+
+#3.3 
+vehicles = ['car', 'bike', 'train', 'plane']
+print(f'I\'d love to take {vehicles[3]}')
+print(f'I like riding my {vehicles[1]}')
+print(f'There\'s too much {vehicles[0]}s in our cities')
