1fcc55cf082231f37fdaefa67c18dbbce00540f6
Nicolas Denoël <info@nicode.io>
2020-10-05 13:44:14 +0200

    Add dynamic title using regex in url

--- myclub_site_root/events/views.py
+++ myclub_site_root/events/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date
+import calendar
 
-def index(request):
-    t = date.today()
-    month = date.strftime(t, '%b')
-    year = t.year
-    title = "MyClub Event Calendar - %s %s" % (month, year)
+def index(request, year, month):
+    year = int(year)
+    month = int(month)
+    if year < 2000 or year > 2099: year = date.today().year
+    month_name = calendar.month_name[month]
+    title = "MyClub Event Calendar - %s %s" % (month_name, year)
     return HttpResponse("<h1>%s</h1>" % title)
 
 
