6dd0b611286d9f9f6b34910ae73d88e9b3f60b81
Nicolas Denoël <info@nicode.io>
2020-10-06 17:10:39 +0200

    Add user, group and password reset option in admin panel

--- myclub_site_root/myclub_site/urls.py
+++ myclub_site_root/myclub_site/urls.py
@@ -15,8 +15,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Add password reset link under login fields in admin panel
+    path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='admin_password_reset',
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView,
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
+
     path('', include('events.urls')),
 ]
