3cdb993c13ddb886167e78e885209f7f0fb864f0
Nicolas Denoël <info@nicode.io>
2020-10-06 23:24:03 +0200

    Add custom validation rules to 'VenueForm'

--- myclub_site_root/events/forms.py
+++ myclub_site_root/events/forms.py
@@ -7,3 +7,12 @@ class VenueForm(ModelForm):
     class Meta:
         model = Venue
         fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        phone = cleaned_data.get("phone")
+        email_address = cleaned_data.get("email_address")
+        if not (phone or email_address):
+            raise forms.ValidationError(
+                "You must enter either a phone number or an email or both."
+            )
