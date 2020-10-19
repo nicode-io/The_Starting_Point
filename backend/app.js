6b50b498677cbec0336091743acf56ab1e80b2f3
Bastien Dewerse <bastien.dewerse@gmail.com>
2020-10-19 16:16:25 +0200

    DB connect
    Connection to DB added with basic get and post to feature it

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+const routes = require('./routes/routes');
+
+app.use('/', routes);
+
+const database = "mongodb+srv://username:123password@cluster0.2sv4t.gcp.mongodb.net/fabulab?retryWrites=true&w=majority";
+const port = 8080;
+
+mongoose
+    .connect(database, {
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        app.listen(port, () => console.log(`Server and Database running on ${port}, http://localhost:${port}`));
+    })
+    .catch((err) => {
+        console.log(err);
+    });
