a83673de66618fd01600b443473412427b442a70
Delvaux <Robby>
2020-10-31 20:34:14 +0100

    bit style
    put a bit style & css in login compenent & fix some routes in backend ^^

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import  './login.css ';
+
 
 
 const LoginC = () =>{
      
         return (
+            
             <section className="log_form">
-                <form action="" method="POST">
-                    <input type="text" name="login" id="login" placeholder="Entrez votre login"/>
-                    <input type="password" name="password" id="password" placeholder="Entrez votre password"/>
-                    <input type="submit" value="Envoyer"/>
+                <form action="#" method="POST" className="log_form">
+                    <input type="text" name="login" id="login" autocomplete="off" placeholder="Entrez votre login"/>
+                    <input type="password" name="password" id="password" autocomplete="off" placeholder="Entrez votre password"/>
+                    <input type="submit" value="Envoyer" id="log_submit"/>
                 </form>
             </section>
         )
     
-}
+};
 
 export default LoginC;
