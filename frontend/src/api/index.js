b9ae47108fdd6c8eef4a7cbf2675fb87641619b2
Mohamed <m.shmayssany@gmail.com>
2020-11-02 01:33:01 +0100

    add User-List & fixing cors

---
+++ frontend/src/api/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8080'
+});
+
+export const insertUser = payload => api.post(`/add-user`, payload);
+export const getAllUsers = () => api.get(`/users`);
+export const updateUserById = (id, payload) => api.put(`/edit-user/${id}`, payload);
+export const deleteUserById = id => api.delete(`/delete-user/${id}`);
+export const getUserById = id => api.get(`/${id}`);
+
+const apis = {
+    insertUser,
+    getAllUsers,
+    updateUserById,
+    deleteUserById,
+    getUserById,
+}
+
+export default apis;
