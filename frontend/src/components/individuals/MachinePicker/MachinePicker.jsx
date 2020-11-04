f67e8fd163fa31a29320570bc039b79b2b55c44a
Delvaux <Robby>
2020-11-04 15:07:44 +0100

    method back make for machine

--- frontend/src/components/individuals/MachinePicker/MachinePicker.jsx
+++ frontend/src/components/individuals/MachinePicker/MachinePicker.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 
+
 export function MachinePicker() {
     return (
         <div>
-            <p>SA MARCHEEEEE</p>
+            <p>SA MARCHEEEEE </p>
         </div>
     );
 }
