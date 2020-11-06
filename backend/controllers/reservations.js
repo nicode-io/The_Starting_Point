1fd66964240243d80368012193af508938897859
Delvaux <Robby>
2020-11-06 12:18:39 +0100

    method on product & reservation make

--- backend/controllers/reservations.js
+++ backend/controllers/reservations.js
@@ -1,20 +1,59 @@
 const Reservation = require('../models/Reservation');
 
-exports.getAllReservation = (req, res) => {
-
+exports.getAllReservation = async(req, res) => {
+    try {
+        const reservations = await Reservation.find();
+        res.json(reservations);
+    }catch(error){
+        console.log(error);
+    }
 }
-exports.getReservation = (req, res) => {
-
+exports.getReservation = async(req, res) => {
+    const reservationId = req.params.reservationId;
+    try{
+        const reservation = await Reservation.findById(reservationId, (reservation) => reservation);
+        res.json(reservation);
+    }catch(error){
+        console.log(error);
+    }
 }
 exports.postReservation = (req, res) => {
-
+    const {name , usernotlogged , userlogged , startdate , enddate} = req.body;
+    try{
+        const reservation = new Reservation({name:name, usernotlogged:usernotlogged , userlogged:userlogged , startdate:startdate , enddate:enddate});
+        reservation.save();
+    }catch(error) {
+        console.log(error);
+    }
 }
 exports.getEditReservation = (req, res) => {
-    
+    // A GERE UNIQUEMENT ADMIN AURA DROIT
 }
 exports.postEditReservation = (req, res) => {
-    
+    const reservationId = req.body.reservationId;
+    const {name,usernotlogged,userlogged,startdate,enddate} = req.body;
+
+    Reservation.findById(reservationId).then((reservation) => {
+        reservation.name = name;
+        reservation.usernotlogged = usernotlogged;
+        reservation.userlogged = userlogged;
+        reservation.startdate = startdate;
+        reservation.enddate = enddate;
+
+        return reservation.save()
+    }).then(() =>{
+        console.log('Reservation Updated');
+    }).catch((error) =>{
+        console.log(error);
+    });
 }
-exports.deleteReservation = (req, res) => {
-    
+exports.deleteReservation = async(req, res) => {
+    const reservationId = req.body.reservationId;
+
+    try {
+        const reservation = await Reservation.findByIdAndDelete(reservationId, (reservation) => reservation);
+        console.log(reservation + 'This reservation has been deleted');
+    }catch(error) {
+        console.log(error);
+    }
 }
