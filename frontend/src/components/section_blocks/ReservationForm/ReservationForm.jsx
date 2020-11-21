d7e7310993c35da285aff0a212c33413898a72b0
Delvaux <Robby>
2020-11-21 14:58:53 +0100

    Calendar ok on reservation

--- frontend/src/components/section_blocks/ReservationForm/ReservationForm.jsx
+++ frontend/src/components/section_blocks/ReservationForm/ReservationForm.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { MachinePicker } from '../..';
 import { FormField } from '../..';
+// IMPORT FOR CALENDAR
+import  { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 export function ReservationForm() {
+    const [startDate, setStartDate] = useState(new Date());
     return (
         <section>
             <form>
@@ -11,6 +17,7 @@ export function ReservationForm() {
                 
                 <FormField label="Nom" type="text" />
                 <FormField label="Email" type="email" />
+                <DatePicker dateFormat="dd/MM/yyyy" selected={startDate} onChange={date => setStartDate(date)} />
                 <p>Avez-vous besoin d'accompagnement ?</p>
                 <div>
                     <FormField label="oui" type="radio" name="Test" />
