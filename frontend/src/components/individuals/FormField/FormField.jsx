73ce848efe862d2c492c299f4f31dbea477d140d
Bastien Dewerse <bastien.dewerse@gmail.com>
2020-11-25 10:51:23 +0100

    Login/Logout in the header

--- frontend/src/components/individuals/FormField/FormField.jsx
+++ frontend/src/components/individuals/FormField/FormField.jsx
@@ -6,21 +6,21 @@ export function FormField(props,{data}) {
     let required = (props.required !== undefined) ? props.required : false;
 
     if (props.type === "textarea") {
-        field = <div>
+        field = <div style={props.style}>
                     <label for={props.label}>{props.label}</label>
                     <textarea name={props.label} onChange={e => props.callback(e.target.value)}/>
                 </div>
     } else if (props.type === "submit" || props.type === "button") {
-        field = <div>
+        field = <div style={props.style}>
                     <input name={props.label} type={props.type} value={props.label} onChange={e => props.callback(e.target.value)}/>
                 </div>
     } else if (props.type === "radio") {
-        field = <div>
+        field = <div style={props.style}>
                     <label for={props.label}>{props.label}</label>
                     <input id={props.label} type={props.type} value={props.label} name={props.name} onChange={e => props.callback(e.target.value)}/>
                 </div>
     } else  {
-        field = <div>
+        field = <div style={props.style}>
                     <label for={props.label}>{props.label}</label>
                     <input name={props.label} type={props.type} required={required} onChange={e => props.callback(e.target.value)}/>
                 </div>
--- frontend/src/components/main_blocks/Header/Header.jsx
+++ frontend/src/components/main_blocks/Header/Header.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import './header.css';
 import { Link, useLocation } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
+import { FormField } from '../..';
 
 const Header = () => {
+
+    let isLogged = false;
+
     return (
         <header className="hea-main">
             <Link to="/" className={`nav-link${useLocation().pathname === "/" ? " is-current" : ""}`}>
                 <img src='https://www.fablab-charleroi.be/wp-content/themes/bootstrapspada4/assets/images/logo-header.png' alt='Logo FabLab'></img>
             </Link>
+
+            <FormField label="Logout" type="button" style={{display: (isLogged) ? 'block' : 'none'}}/>
+            <Link to="/login" className={`nav-link${useLocation().pathname === "/login" ? " is-current" : ""}`} style={{display: (!isLogged) ? 'block' : 'none'}}>Login</Link>
+
             <Menu isOpen={true}>
                 <h1>Menu</h1>
                 <Link to="/" className={`nav-link${useLocation().pathname === "/" ? " is-current" : ""}`}><span></span> Home</Link>
