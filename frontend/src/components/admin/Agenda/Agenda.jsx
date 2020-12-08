0696ea01953b398a7e0f1438446ea887fd27ff31
Bastien Dewerse <bastien.dewerse@gmail.com>
2020-12-08 16:10:15 +0100

    Modal shows reservations start and end
    And some styles

--- frontend/src/components/admin/Agenda/Agenda.jsx
+++ frontend/src/components/admin/Agenda/Agenda.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import api from '../../../api';
 import { FormField, Modal } from "../../commons";
@@ -12,27 +12,41 @@ export function Agenda(props) {
         startdate: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 15),
         enddate: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 16),
         _id: 42,
+        machine: {name: "Imprimante 3D"},
     },{
         startdate: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 16),
         enddate: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 17),
         _id: 43,
+        machine: {name: "Decoupeuse"},
     },{
         startdate: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 15, 30),
         enddate: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 16, 30),
         _id: 44,
+        machine: {name: "Petit matériel"},
     },];
 
     // Monday -> Friday // 9h30 -> 17h30 // 5 * 16 time periods of 30min
     const WEEK = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"]
     const WORKINGDAYS = [1, 2, 3, 4, 5]; // working days of the week matching Date.getDay() ex: [1, 2, 4, 5] for Monday, Thuesday, Thursday, Friday
-    const WORKINGHOURS = [570, 1050]; // [starting hour, ending hour] 570 = 9h30, 1050 = 17h30, in minutes
+    const WORKINGHOURS = [570, 1050]; // [starting hour, ending hour] in minutes, 570 = 9h30, 1050 = 17h30
     const PERIOD = 30; // time frame for the agenda, in minutes
 
-    const [selectedDay, setSelectedDay] = useState(new Date());
+    const selectedDayInit = () => {
+        let date = new Date();
+        date.setHours(0, 0, 0, 0);
+        return date;
+    }
+
+    const [selectedDay, setSelectedDay] = useState(selectedDayInit());
     const [agenda, setAgenda] = useState([]);
 
     const [isModalVisible, setisModalVisible] = useState(false);
-    const [modalReservations, setModalReservations] = useState([]);
+    const [modalData, setModalData] = useState({});
+
+    const dateToString = (date) => {
+        let string = date.getHours() + "h" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes());
+        return string;
+    }
 
     /**
      * Creates an array containing every working day of a week in {Date} format
@@ -66,21 +80,20 @@ export function Agenda(props) {
         week.map((date) => {
             let day = [];
             for (let i = workinghours[0]; i < workinghours[1]; i = i + period) {
-                let periodDate = new Date(date);
-                periodDate.setHours(Math.floor(i / 60));
-                periodDate.setMinutes((i / period % 2) * period);
+                let startDate = new Date(date.getTime() + (i * 60000));
+                let endDate = new Date(startDate.getTime() + (period * 60000));
+                let name = WEEK[date.getDay()] + " " + dateToString(startDate) + " - " + dateToString(endDate);
                 let periodReservations = [];
                 reservations.map((reservation) => {
-                    (periodDate.getTime() >= reservation.startdate && periodDate.getTime() < reservation.enddate) 
-                        && periodReservations.push(reservation._id);
+                    (startDate.getTime() >= reservation.startdate && startDate.getTime() < reservation.enddate) 
+                        && periodReservations.push(reservation);
                 })
-                day.push({name: periodDate, reservations: periodReservations})
+                day.push({startDate: startDate, endDate: endDate, name: name, reservations: periodReservations})
             }
             agenda.push(day);
         })
         console.log(agenda);
         setAgenda(agenda);
-        // return agenda;
     }
 
     /**
@@ -88,6 +101,8 @@ export function Agenda(props) {
      * 
      * @param {Array} workinghours an array of {Number} containing the working hours range in minutes [starting hour, ending hour]
      * @param {Number} period the time period between each timestamp
+     * 
+     * @return {Array} 
      */
     const getTimestamps = (workinghours, period) => {
         let timestamps = [];
@@ -105,13 +120,41 @@ export function Agenda(props) {
         )
     }
 
-    const displayReservations = (reservations) => {
-        setModalReservations(reservations);
+    const displayPeriod = (period) => {
+        setModalData({
+            title: period.name,
+            content: createModalContent(period),
+        });
         setisModalVisible(true);
-        // return (<Modal />);
-        // console.log(reservations);
     }
 
+    const createModalContent = (period) => {
+        console.log(period)
+        return (
+            (period.reservations.length > 0)
+            ? period.reservations.map((reservation) => {
+                    return (
+                        <div className="ag-reservationContainer">
+                            <div className={(period.startDate.getTime() === reservation.startdate.getTime()) ? "ag-reservationBorder" : "ag-reservationBorder ag-reservationFull"}>
+                                <p>{dateToString(reservation.startdate)}</p>
+                            </div>
+                            <div className="ag-reservation">
+                                <p>ID: {reservation._id}</p>
+                                <p>Machine: {reservation.machine.name}</p>
+                                {/* <p>Début: {dateToString(reservation.startdate)} - Fin: {dateToString(reservation.enddate)}</p> */}
+                            </div>
+                            <div className={(period.endDate.getTime() === reservation.enddate.getTime()) ? "ag-reservationBorder" : "ag-reservationBorder ag-reservationFull"}>
+                                <p>{dateToString(reservation.enddate)}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            : <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+        )
+    }
+                        
     // async function getItemById(type ,id){
     //     try {
     //         await api.getById(`/${type}`, id)
@@ -167,27 +210,26 @@ export function Agenda(props) {
                         </div>
                         {agenda.map((day) => {
                             return (
-                                <div className="ag-day" key={WEEK[day[0].name.getDay()]}>
+                                <div className="ag-day" key={WEEK[day[0].startDate.getDay()]}>
                                     <div className="ag-period ag-period-title">
-                                        <p>{WEEK[day[0].name.getDay()].substring(0,3)}</p>
-                                        <p>{day[0].name.getDate() + "/" + (day[0].name.getMonth() + 1)}</p>
-                                        {/* + "/" + day[0].name.getFullYear() */}
+                                        <p>{WEEK[day[0].startDate.getDay()].substring(0,3)}</p>
+                                        <p>{day[0].startDate.getDate() + "/" + (day[0].startDate.getMonth() + 1)}</p>
                                     </div>
                                     {day.map((period) => {
-                                            return (
-                                                <div className={(period.reservations.length > 0) ? "ag-period ag-period-full" : "ag-period ag-period-empty"} 
-                                                key={WEEK[day[0].name.getDay()] + "-" + period.name.getHours() + "h" + period.name.getMinutes()}
-                                                onClick={() => displayReservations(period.reservations)}>
-                                                    {period.reservations.length}
-                                                </div>
-                                            )
+                                        return (
+                                            <div className={(period.reservations.length > 0) ? "ag-period ag-period-full" : "ag-period ag-period-empty"} 
+                                            key={period.name}
+                                            onClick={() => displayPeriod(period)}>
+                                                {period.reservations.length}
+                                            </div>
+                                        )
                                     })}
                                 </div>
                             )
                         })}
                     </div>
                 }
-                <Modal isVisible={isModalVisible} setIsVisible={setisModalVisible} data={modalReservations} />
+                <Modal isVisible={isModalVisible} setIsVisible={setisModalVisible} data={modalData} />
             </section>
     )
 }
--- frontend/src/components/commons/Modal/Modal.jsx
+++ frontend/src/components/commons/Modal/Modal.jsx
@@ -1,5 +1,4 @@
 import React, { Fragment, useState }from 'react';
-// import { FormField } from '..';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import './modal.css';
@@ -8,27 +7,40 @@ import './modal.css';
  * 
  * @param {Boolean} props.isVisible 
  * @param {Function} props.setIsVisible
- * @param {Array} props.data
+ * @param {Object} props.data = { title: {String}, items: {Array} }
  */
 const Modal = (props) => {
     return (
         <Fragment>
             {(props.isVisible)
                 ? <div className="modalContainer">
-                    <button className="modalButton" onClick={() => props.setIsVisible(false)}><FontAwesomeIcon icon={faTimesCircle} /></button>
-                    <div className="dataContainer">
-                        {(props.data.length > 0)
-                            ? props.data.map((item) => {
+                    <div className="modalHeader">
+                        <button className="modalButton" onClick={() => props.setIsVisible(false)}><FontAwesomeIcon icon={faTimesCircle} /></button>
+                        <h6>{props.data.title}</h6>
+                        <button className="modalButton" onClick={() => props.setIsVisible(false)}><FontAwesomeIcon icon={faTimesCircle} /></button>
+                    </div>
+                    <div className="modalData">
+                        {props.data.content}
+                        {/* {(props.data.items.length > 0)
+                            ? props.data.items.map((item) => {
                                     return (
-                                        <p>{item}</p>
+                                        <div className="modalItem">
+                                            <div className="itemLeft"></div>
+                                            <div className="itemCenter">
+                                                <p>{item._id}</p>{console.log(item)} */}
+                                                {/* <p>{item.startdate}</p>
+                                                <p>{item.enddate}</p> */}
+                                                {/* <p>{item.machine.name}</p>
+                                            </div>
+                                            <div className="itemRight"></div>
+                                        </div>
                                     )
                                 })
                             : <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                             </p>
-                        }
+                        } */}
                     </div>
-                    <button className="modalButton" onClick={() => props.setIsVisible(false)}><FontAwesomeIcon icon={faTimesCircle} /></button>
                 </div>
                 : null
             }
