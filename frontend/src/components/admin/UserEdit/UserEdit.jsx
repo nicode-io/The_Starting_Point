a9f3fa9da5fab2afe857e1f1f0959c3301015d01
Sim <duquaine.simon@gmail.com>
2020-12-16 16:21:58 +0100

    fixed style of user edit, add not signed up yet ?

--- frontend/src/components/admin/UserEdit/UserEdit.jsx
+++ frontend/src/components/admin/UserEdit/UserEdit.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import api from '../../../api';
 import { useLocation } from 'react-router-dom';
 import { FormField } from "../../commons";
+import './userEdit.css';
 
 export function UserEdit() {
     let params = useLocation().pathname.split('/');
-    let id = params[3]; 
+    let id = params[3];
     let user;
     const [idUser, setIdUser] = useState(params[3]);
     const [firstname, setFirstname] = useState('');
@@ -16,7 +17,7 @@ export function UserEdit() {
     const [type, setType] = useState('');
 
     const getUserById = async () => {
-        user = await api.getById('/user',id);
+        user = await api.getById('/user', id);
         setFirstname(user.data.firstname);
         setLastname(user.data.lastname);
         setEmail(user.data.email);
@@ -30,34 +31,35 @@ export function UserEdit() {
     }, []);
 
     const handleSubmit = () => {
-            
-        api.updateById('/edit-user',{
-            id : idUser,
-            firstname : firstname,
-            lastname : lastname,
-            email : email,
-            tel : tel,
+
+        api.updateById('/edit-user', {
+            id: idUser,
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            tel: tel,
             company: company,
             type: type
-        }).then((response) =>{
+        }).then((response) => {
             console.log(response);
-            
+
         }, (error) => {
             console.log(error);
         });
     }
-    
+
     return (
-        <section className={"main-edit-user"}>
+        <section className="main-edit-user">
+            <section class="register-header"><h1>Modification</h1></section>
             <article className="white-container">
                 <section>
                     <form onSubmit={handleSubmit}>
-                        <FormField type="text" value={firstname} label="Nom" callback={c=> setFirstname(c)}/>
-                        <FormField type="text" value={lastname} label="Prénom" callback={c=> setLastname(c)}/>
-                        <FormField type="text" value={email} label="Email" callback={c=> setEmail(c)}/>
-                        <FormField type="text" value={tel} label="Tel" callback={c=> setTel(c)}/>
-                        <FormField type="text" value={company} label="Company" callback={c=> setCompany(c)}/>
-                        <FormField type="text" value={type} label="Type" callback={c=> setType(c)}/>
+                        <FormField type="text" value={firstname} label="Nom" callback={c => setFirstname(c)} />
+                        <FormField type="text" value={lastname} label="Prénom" callback={c => setLastname(c)} />
+                        <FormField type="text" value={email} label="Email" callback={c => setEmail(c)} />
+                        <FormField type="text" value={tel} label="Tel" callback={c => setTel(c)} />
+                        <FormField type="text" value={company} label="Company" callback={c => setCompany(c)} />
+                        <FormField type="text" value={type} label="Type" callback={c => setType(c)} />
                         <FormField type="submit" label="Update" />
                     </form>
                 </section>
--- frontend/src/components/admin/UsersForm/UsersForm.jsx
+++ frontend/src/components/admin/UsersForm/UsersForm.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import api from '../../../api';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMinusSquare, faPenSquare, faPlusSquare} from '@fortawesome/free-solid-svg-icons'
+import { faMinusSquare, faPenSquare, faPlusSquare } from '@fortawesome/free-solid-svg-icons'
 import { FormField } from '../../commons';
+import "./userForm.css";
 
 
 export function UsersForm() {
@@ -14,25 +15,26 @@ export function UsersForm() {
     const [type, setType] = useState('text');
 
 
-    useEffect( () => {
-           fetchData();
-    
+    useEffect(() => {
+        fetchData();
+
     }, []);
-        async function fetchData() {
+    async function fetchData() {
         setIsLoaded(true);
         await api.getAll('/users')
-        .then((data) => {
-            setIsLoaded(true);
-            setUsers(data.data);
-            
-        },
-        (error) => {
-            setIsLoaded(true);
-            setError(error);
-        }
-        )}
- 
-    async function deleteItemById(type, id, name){
+            .then((data) => {
+                setIsLoaded(true);
+                setUsers(data.data);
+
+            },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }
+
+    async function deleteItemById(type, id, name) {
         try {
             let txt;
             if (window.confirm(`Voulez vous supprimer un(e) ${type}`)) {
@@ -40,24 +42,24 @@ export function UsersForm() {
             } else {
                 txt = false;
             }
-            if(txt){
+            if (txt) {
                 await api.deleteById(`/delete-${type}`, id)
-                .then((res) => {
-                    if (res.status === 200) {
-                        setMessage(`${name} supprimé(e)`);
-                        fetchData(`${type}`);
-                    } else {
-                        setMessage(`Probleme`);
-                    }
-                })
-            }else{
+                    .then((res) => {
+                        if (res.status === 200) {
+                            setMessage(`${name} supprimé(e)`);
+                            fetchData(`${type}`);
+                        } else {
+                            setMessage(`Probleme`);
+                        }
+                    })
+            } else {
                 console.log('delete not CONFIRM !!!');
             }
-        } catch(error) {
+        } catch (error) {
             console.log(error);
         }
     }
-    
+
     if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -65,44 +67,38 @@ export function UsersForm() {
     } else {
 
         return (
-            <section>
-                {message}
-                <table className="table w-50 text-center">
-                    <thead className="thead-dark">
-                        <tr>
-                        <th style={{width: '25%'}} scope="col">Nom</th>
-                        <th style={{width: '25%'}} scope="col">Prénom</th>
-                        <th style={{width: '25%'}} scope="col">Email</th>
-                        <th style={{width: '25%'}} scope="col">Tel</th>
-                        <th style={{width: '25%'}} scope="col">Company</th>
-                        <th style={{width: '25%'}} scope="col">Type</th>
-                        <th style={{width: '25%'}} scope="col">Modifier</th>
-                        <th style={{width: '25%'}} scope="col">Supprimer</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {users.map(user => (
-                        <tr key={"list-" + user._id}>
-                        <td>{user.firstname}</td>
-                        <td>{user.lastname}</td>
-                        <td>{user.email}</td>
-                        <td>{user.tel}</td>
-                        <td>{user.company}</td>
-                        <td>{user.usertype}</td>
-                        <td>
-                            <Link to={`user-edit/${user._id}`}>
-                                <FontAwesomeIcon icon={faPenSquare} size="2x" />
-                            </Link>
-                        </td>
-                        <td>
-                            <FormField type="button" callback={() => deleteItemById('user', user._id, user.email)}>
-                                <FontAwesomeIcon icon={faMinusSquare} size="2x" />
-                            </FormField>
-                        </td>
-                        </tr>
-                        ))}
-                    </tbody>
-                </table>
+            <section className="d-flex justify-content-center align-items-center flex-column mt-3">
+                <section className={"white-container"}>
+                    <h2>Utilisateurs</h2>
+                    <table className="table">
+                        <thead className="banner-list">
+                            <tr>
+                                <th>Nom</th>
+                                <th>Prénom</th>
+                                <th>Modifier</th>
+                                <th>Supprimer</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map(user => (
+                                <tr key={"list-" + user._id}>
+                                    <td>{user.firstname}</td>
+                                    <td>{user.lastname}</td>
+                                    <td>
+                                        <Link to={`user-edit/${user._id}`}>
+                                            <FontAwesomeIcon icon={faPenSquare} size="2x" />
+                                        </Link>
+                                    </td>
+                                    <td>
+                                        <FormField type="button" callback={() => deleteItemById('user', user._id, user.email)}>
+                                            <FontAwesomeIcon icon={faMinusSquare} size="2x" />
+                                        </FormField>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </section>
             </section>
         );
     }
