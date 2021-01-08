8d22edf3b2ed63da6136a027358bb37d47c91630
Nicolas Denoël <info@nicode.io>
2021-01-08 15:44:18 +0100

    Add errors management middleware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,24 @@
+// Middleware to manage bad URL's requests
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+}
+
+// Middleware to manage errors
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500  : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    })
+}
+
+// Middleware to log requested urls
+const originalUrl = (req, res, next) => {
+    console.log(req.originalUrl);
+    next();
+} 
+
+export { notFound, errorHandler, originalUrl }
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { notFound, errorHandler, originalUrl } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js';
 
@@ -18,27 +19,10 @@ app.get('/', (req, res) => {
 // Use router from productRoutes
 app.use('/api/products', productRoutes);
 
-app.use((req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-})
-
-// Middleware to manage errors
-app.use((err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500  : res.statusCode
-    res.status(statusCode)
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-    })
-})
-
-// Middleware to log requested url
-app.use((req, res, next) => {
-    console.log(req.originalUrl);
-    next();
-})
+// Middleware calls
+app.use(notFound);
+app.use(errorHandler);
+app.use(originalUrl);
 
 const PORT = process.env.PORT || 5000;
 
