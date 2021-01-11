6a30574b1108b59f01eb9b242dfee76a2b5230c8
Nicolas Denoël <info@nicode.io>
2021-01-11 08:12:16 +0100

    End of express module

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-const http = require("http");
+// CORE modules
+const path = require("path");
+// 3rd-Party Packages
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const routes = require("./routes");
+// Create express app
+const app = express();
 
-const server = http.createServer(routes);
+// Import Express routers
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-server.listen(5000);
+// Parse request body (req.body)
+app.use(bodyParser.urlencoded({ extended: false })); // Body Parser manage the 'next' operation
+
+// Server static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Use Express routers
+app.use("/admin", adminRoutes); // Wil add /admin to paths defined in the router
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(5000);
