12ba732aa34d21f47516f1954d400e11fe971f7c
Nicolas Denoël <info@nicode.io>
2021-01-12 16:05:19 +0100

    End of SQL / Sequelize
    Skeleton for an e-shop with followings:
    - Products CRUD
    - Cart
    - Order validation
    - User associations (no login management)

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
   });
 };
 
@@ -13,9 +13,35 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+
+  // Magic method created by Sequelize when we create an association
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+    })
+    .then(result => {
+      // console.log(result)
+      console.log('Created Product Successfully');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
+
+  // Create a new product whith classic no-associations 
+  // Product.create({
+  //   title: title,
+  //   price: price,
+  //   imageUrl: imageUrl,
+  //   description: description
+  // })
+  //   .then(result => {
+  //     // console.log(result)
+  //     console.log('Created Product Successfully');
+  //     res.redirect('/admin/products');
+  //   })
+  //   .catch(err => console.log(err))
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -24,17 +50,22 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-  });
+
+  req.user
+    .getProducts({ where: { id: prodId } }) // Magic method created by Sequelize when associations defined
+    .then(products => {
+      const product = products[0]; // Query always returns an array
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch(err => console.log(err))
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -43,23 +74,45 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+
+  Product.findByPk(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      return product.save(); // Return promise value to allow second ".then" and avoid nesting
+    })
+    .then(result => {
+      console.log('Product Updated !');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err)) // Will catch errors of multiple promises
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  req.user
+    .getProducts() // Magic method created by Sequelize when associations defined
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products',
+      });
+    })
+    .catch(err => console.log(err))
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  Product.findByPk(prodId)
+    .then(product => {
+      return product.destroy();
+    })
+    .then(result => {
+      console.log('Product deleted');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err))
 };
