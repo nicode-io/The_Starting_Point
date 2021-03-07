55452937eccefc5daab72bc15c5d7e1d03390156
Niko Den <info@nicode.io>
2021-03-07 23:50:43 +0100

    Classes and Unit Tests
    After an introduction on how is the code structured in a C# project, we create some classes and we've done our first unit test. Project is a basic Grade Book.

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GradeBook // Namespace are usefull to get class / method outisde the Global namespace, which is dangerous and can create conflicts
+{
+    class Program
+    {
+        static void Main(string[] args) // Entry point of application is method called "main" - Method's parameters are a string array called "args"
+        {
+            // ! var keep type of value it is assigned, it's not like in JS !
+            var book = new Book("Micholas' Grade Bookclear");
+            book.AddGrade(85.1);
+            book.AddGrade(77.2);
+            book.AddGrade(97.2);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The highest grade is {stats.High:N1}");
+            Console.WriteLine($"The lowest grade is {stats.Low:N1}");
+            Console.WriteLine($"The average grade is {stats.Average:N2}");
+        }
+    }
+}
+
