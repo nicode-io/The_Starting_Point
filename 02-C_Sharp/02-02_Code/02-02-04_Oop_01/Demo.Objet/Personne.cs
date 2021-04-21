bdd708f5c6d0c5ba755bdfb9633455576fb7fc49
Nicolas Denoël <info@nicode.io>
2021-04-21 10:18:05 +0200

    C# - Add Code Oop-01

---
+++ 02-C_Sharp/02-02_Code/02-02-04_Oop_01/Demo.Objet/Personne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Objet
+{
+    class Personne
+    {
+        private string nom;
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        private string prenom;
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+
+        private DateTime dateDeNaiss;
+        public DateTime DateDeNaiss
+        {
+            get { return dateDeNaiss; }
+            set { dateDeNaiss = value; }
+        }
+    }
+}
