c7a8a09e528ad0fed51c4e4b2ea1ce842d0265c9
Nicolas Denoël <info@nicode.io>
2021-05-13 12:06:34 +0200

    Add Entityframewrok Exos

---
+++ 03-ASP_dotNet/03-Entity_Framework/02-Code/01-EntityFrameworkExos/EfExo/EntitiesConfiguration/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfExo.EntitiesConfiguration
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string MainActor { get; set; }
+        public string Category { get; set; }
+        public string Director { get; set; }
+    }
+}
