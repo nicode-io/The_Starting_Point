41020260a93a9d64ba5d509c498a1ca423baf894
Nicolas Denoël <info@nicode.io>
2021-05-15 20:27:48 +0200

    Game Over : Add results layout

--- 02-NumberGuesser/Screens/GameOver.js
+++ 02-NumberGuesser/Screens/GameOver.js
@@ -8,14 +8,18 @@ const GameOver = props => {
     return (
         <View style={styles.screen}>
             <Card style={styles.container}>
-                <Text style={styles.title}>Your Number was: {props.userNumber}</Text>
                 <Text style={styles.title}>The game is over !</Text>
+                <Text style={styles.title}>Your Number was: {props.userNumber}</Text>
                 <Text style={styles.text}>Computer rounds :</Text>
                 <Number>{props.rounds}</Number>
                 <Text style={styles.text}>to find your number</Text>
             </Card>
-            <Card style={styles.container}>
-                <Button title='Play Again' onPress={props.onStartNewGame} />
+            <Card style={styles.replayContainer}>
+                <Button
+                    title="Play Again"
+                    onPress={props.onStartNewGame}
+                    color={Colors.yellow}
+                />
             </Card>
         </View>
     )
@@ -24,23 +28,32 @@ const GameOver = props => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center'
     },
     container: {
         width: 300,
         maxWidth: '80%',
-      backgroundColor: Colors.fuchsia,
+        marginVertical: 30,
+        backgroundColor: Colors.fuchsia,
     },
     title: {
         textAlign: 'center',
         color: Colors.yellow,
         fontSize: 20,
-        fontWeight:'700'
+        fontWeight: '700'
     },
     text: {
+        marginTop: 10,
         textAlign: 'center',
-        color: 'white'
+        color: 'white',
+        fontSize: 18
+    },
+    replayContainer: {
+        width: 300,
+        maxWidth: '60%',
+        marginVertical: 10,
+        backgroundColor: Colors.fuchsia,
     }
 });
 
